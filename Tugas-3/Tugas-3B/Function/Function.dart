da3f1675f51542d2771b32692e6a2b659a7e2144
fadlifadli <fadli.azhari.elka@gmail.com>
2024-02-21 14:46:28 +0700

    updateTugas3B

---
+++ Tugas-3/Tugas-3B/Function/Function.dart
@@ -0,0 +1,13 @@
+// No. 1 
+// Tulislah sebuah function dengan nama teriak() yang mengembalikan nilai “Halo Sanbers!” yang kemudian dapat ditampilkan di console.
+
+// print(teriak()) // "Halo Sanbers!" 
+
+
+String teriak() {
+  return "Halo Sanbers!";
+}
+
+void main() {
+  print(teriak()); // Output: "Halo Sanbers!"
+}
