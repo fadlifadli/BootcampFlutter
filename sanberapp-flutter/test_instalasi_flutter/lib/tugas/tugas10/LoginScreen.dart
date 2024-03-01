3ecc64927f59dbcae49d395592bf86a883638885
fadlifadli <fadli.azhari.elka@gmail.com>
2024-03-01 18:38:23 +0700

    updateTugas11-o

--- sanberapp-flutter/test_instalasi_flutter/lib/tugas/tugas10/LoginScreen.dart
+++ sanberapp-flutter/test_instalasi_flutter/lib/tugas/tugas10/LoginScreen.dart
@@ -48,15 +48,42 @@ class _LoginScreenState extends State<LoginScreen> {
                 padding: EdgeInsets.all(10),
                 child: TextField(
                   controller:nameController,
+                  obscureText: true,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Password"
                   ),
                 ),
               ),
+              TextButton(onPressed:(){
+
+              },
+              child: Text("Forgot Password"),
+              ),
+              Container(
+                height: 50,
+                padding: EdgeInsets.fromLTRB(10,0,10,0),
+                child: ElevatedButton(
+                  style: raisedButtonStyle,
+                  child: Text("Login"),
+                  onPressed:(){
+
+                  }
+                )
+              )
           ],
           )
         ),
     );
   }
 }
+
+final ButtonStyle raisedButtonStyle= ElevatedButton.styleFrom(
+  onPrimary:Colors.grey[300],
+  primary: Colors.blue[300],
+  minimumSize: Size(88, 36),
+  padding: EdgeInsets.symmetric(horizontal: 16),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(6)),
+  ),
+);
