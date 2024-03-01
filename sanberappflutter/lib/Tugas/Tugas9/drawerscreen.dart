9fbb852f2c8988cc34219970394e3f2512877aec
fadlifadli <fadli.azhari.elka@gmail.com>
2024-03-01 09:15:11 +0700

    tugas9

---
+++ sanberappflutter/lib/Tugas/Tugas9/drawerscreen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class DrawerScreen extends StatefulWidget {
+  @override 
+  _DrawwerScreenState createState() => _DrawwerScreenState();
+}
+
+class _DrawwerScreenState extends State<DrawerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Muhammad Fadhli Azhari Chadiri"),
+            currentAccountPicture:
+                CircleAvatar(backgroundImage: AssetImage("assets/img/hilmy.jpg")),
+                accountEmail: Text("fadli.azhari.elka@gmail.com"),
+          ),
+          DrawerListTile(
+            iconData: Icons.group,
+            title: "NewGroup",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.lock,
+            title: "New Secret Group",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.notifications,
+            title: "New Channel Chat",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.contacts,
+            title: "contacts",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.bookmark_border,
+            title: "Saved Message",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.phone,
+            title: "Calls",
+            onTilePressed: () {},
+          )
+        ],
+      ));
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  final IconData? iconData;
+  final String? title;
+  final VoidCallback? onTilelPressed;
+
+  const DrawerListTile(
+    {Key? key, this.iconData, this.tile, this.onTilePressed})
+    : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTilePressed,
+      dense: true,
+      leading: Icon(iconData),
+      title: Text(
+        title!,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+}
---
+++ sanberappflutter/lib/Tugas/Tugas9/telegram.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'DrawerScreen.dart';
+import 'Chart_model.dart';
+
+class Telegram extends StatefulWidget {
+  @override
+  _TelegramState createState() => _TelegramState();
+}
+
+class _TelegramState extends State<Telegram> {
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: Text("Telegram"),
+        actions: <Widget> [
+          Padding(
+            padding: const EdgeInsets.all(8.0)
+            child: Icon(Icons.search),
+          )
+        ],
+      ),
+      drawer: DrawerScreen(),
+      body: ListView.separated(
+        itemBuilder:(ctx,i) {
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 28,
+              backgroundImage: NetworkImage(items[i].profileUrl!),
+            ),
+            title: Text(
+              items[i].name!,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Subtitle: Text(items[i].message!),
+            trailing: Text(items[i].time!),
+          );
+        },
+        separatorBuilder: (ctx, i){
+          return Divider();
+        },
+        itemCount: items. length),
+      floatingActionButton: FloatingActionButton(
+        child: icon(Icons.create, color: Colors.white),
+        backgroundColor: Color(0xFF65a9e0),
+        onPressed: () {},
+      ),
+    );
+  }
+}
