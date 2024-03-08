929f2ac2e4f2346db8d952298d1b9fc319db8d34
fadlifadli <fadli.azhari.elka@gmail.com>
2024-03-08 08:02:55 +0700

    tugas14

--- sanberapp-flutter/test_instalasi_flutter/lib/tugas/tugas14/get_data_screen.dart
+++ sanberapp-flutter/test_instalasi_flutter/lib/tugas/tugas14/get_data_screen.dart
@@ -1,3 +1,39 @@
 import '../tugas14/controller/appcontroller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+class GetDataScreenStateManagement extends StatelessWidget {
+  const GetDataScreenStateManagement ({Key? key}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.put(AppController());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("State Management GetX"),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Obx(() {
+            return controller.postloading.value
+            ? Center(
+              child: CircularProgressIndicator(),
+            )
+            : ListView.builder(
+              itemCount: controller.getposts.length,
+              itemBuilder: (context, index) {
+                var item = controller.getposts[index];
+                return Card (
+                  child: ListTile(
+                    title: Text(item.title),
+                    subtitle: Text(item.body),
+                    leading: Text(item.id.toString()),
+                  ),
+                );
+              });
+          }))
+        ],
+      ),
+    );
+  }
+}
